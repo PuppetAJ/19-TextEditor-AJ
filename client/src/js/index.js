0a53491592b6920340db736e50fe759aace25a16
Egg <adrianjimenez1950@gmail.com>
2022-11-15 17:22:35 -0500

    Made indexdb post / create requests functional

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -31,3 +31,10 @@ if ('serviceWorker' in navigator) {
 } else {
   console.error('Service workers are not supported in this browser.');
 }
+
+// const rem = async function() {
+//   const dbs = await window.indexedDB.databases()
+//   dbs.forEach(db => { window.indexedDB.deleteDatabase(db.name) })
+// }
+
+// rem();
